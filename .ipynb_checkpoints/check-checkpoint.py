23aa2754497e20f4eccb100ec3f7a5caf8aebd65
Hariom Kurmi <hariom_k@bt.iitr.ac.in>
2026-02-17 17:58:48 +0530

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/check-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-for i in range(torch.cuda.device_count()):
-    props = torch.cuda.get_device_properties(i)
-    print(f"GPU {i}: {props.name}, Memory: {props.total_memory / 1024**3:.2f} GB")
-
